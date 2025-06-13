94c2c2896ae6d7c213aa13ce3d4c199dc4c5533f
AyushSharma113 <techtinkerzone9@gmail.com>
2025-06-13 20:44:08 +0530

    define pagination functionalitites

--- pagination_project/script.js
+++ pagination_project/script.js
@@ -7,3 +7,56 @@ console.log(sideNav);
 toggleBtn.addEventListener("click", () => {
   sideNav.classList.toggle("hidden");
 });
+
+const cardsPerPage = 4; // Number of cards to show per page
+const dataContainer = document.getElementById("pagination");
+const pagination = document.getElementById("buttonConatainer");
+const prevButton = document.getElementById("prev");
+const nextButton = document.getElementById("next");
+const pageNumbers = document.getElementById("page-numbers");
+const pageLinks = document.querySelectorAll(".page-link");
+
+console.log(dataContainer, "dataContainer");
+
+const cards = Array.from(dataContainer.getElementsByClassName("card"));
+console.log(cards);
+
+const totalPages = Math.ceil(cards.length / cardsPerPage);
+let currentPage = 1;
+
+function displayPage(page) {
+  const startIndex = (page - 1) * cardsPerPage;
+  const endIndex = startIndex + cardsPerPage;
+
+  cards.forEach((card, index) => {
+    if (index >= startIndex && index < endIndex) {
+      card.style.display = "block";
+    } else {
+      card.style.display = "none";
+    }
+  });
+}
+
+// update pagination function
+function updatePagination() {
+  pageNumbers.textContent = `Page ${currentPage} of ${totalPages}}`;
+  prevButton.disabled = currentPage === 1;
+  nextButton.disabled = currentPage === totalPages;
+}
+
+prevButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (currentPage > 1) {
+    currentPage--;
+    displayPage(currentPage);
+  }
+});
+nextButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (currentPage < totalPages) {
+    currentPage++;
+    displayPage(currentPage);
+  }
+});
+
+displayPage(currentPage);
